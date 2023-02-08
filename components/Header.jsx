a3020a58b9d2655757b34f178f51f78efef6c29a
Daniel Yun <danielyun@Daniels-Air.fios-router.home>
2023-02-08 11:08:56 -0500

    draft#1: navbar, post card

---
+++ components/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import Link from 'next/Link'
+import Styles from '@/styles/header.module.scss'
+// import { getCategories } from '../services'
+
+
+const Header = () => {
+    const [categories, setCategories] = useState([
+        {name: "dummy", slug:"dumslg"},
+        {name: "web dev", slug:"web dev"}
+    ])
+
+    // useEffect(()=>{
+    //     getCategories()
+    //         .then((newCategories) => setCategories(newCategories))
+    // }, [])
+
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='border-b w-full inline-block border-black pt-8 pb-5'>
+                <div className='md:float-left block'>
+                    <Link href="/">
+                        <span className={Styles.logo}>
+                            CodingHim
+                        </span>
+                    </Link>
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-4 align-middle text-black text-2xl ml-4 cursor-pointer'>
+                                {category.name}
+                            </span>
+                        </Link>
+
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { Layout } from '../components'
+
+import '../styles/globals.scss'
+import 'tailwindcss/tailwind.css'
+
+export default function App({ Component, pageProps }) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
+}
